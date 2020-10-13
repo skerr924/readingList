b6484efe2f09e002aa169fb6617f6735cbc6bdd4
Sarah Kerr <sarah.kerr93@gmail.com>
2020-10-13 13:31:12 -0400

    deploying

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import { Router, Switch, Route } from "react-router-dom";
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
     <Router>
